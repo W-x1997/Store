f0bff949a0fbfdb3633e361f8344cedde9dea5d0
wx <421980589@qq.com>
2019-07-18 22:46:13 +0800

    ""

---
+++ src/com/weixin/store/dao/Imp/GoodsDaoImp.java
@@ -0,0 +1,34 @@
+package com.weixin.store.dao.Imp;
+
+import com.weixin.store.dao.GoodsDao;
+import com.weixin.store.domain.Goods;
+
+import java.util.List;
+
+public class GoodsDaoImp implements GoodsDao {
+
+    @Override
+    public Goods findByPK(long pk) {
+        return null;
+    }
+
+    @Override
+    public List<Goods> findAll() {
+        return null;
+    }
+
+    @Override
+    public void create(Goods goods) {
+
+    }
+
+    @Override
+    public void modify(Goods goods) {
+
+    }
+
+    @Override
+    public void remove(String pk) {
+
+    }
+}
