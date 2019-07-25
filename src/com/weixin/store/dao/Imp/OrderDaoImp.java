5ca186341a0d0294bcb6023926aab941f6b925ca
wx <421980589@qq.com>
2019-07-25 22:03:08 +0800

    ""

--- src/com/weixin/store/dao/Imp/OrderDaoImp.java
+++ src/com/weixin/store/dao/Imp/OrderDaoImp.java
@@ -1,4 +1,34 @@
 package com.weixin.store.dao.Imp;
 
-public class OrderDaoImp {
+import com.weixin.store.dao.OrderDao;
+import com.weixin.store.domain.Orders;
+
+import java.util.List;
+
+public class OrderDaoImp implements OrderDao {
+
+    @Override
+    public Orders findByPK(String pk) {
+        return null;
+    }
+
+    @Override
+    public List<Orders> findAll() {
+        return null;
+    }
+
+    @Override
+    public void create(Orders orders) {
+
+    }
+
+    @Override
+    public void modify(Orders orders) {
+
+    }
+
+    @Override
+    public void remove(String pk) {
+
+    }
 }
--- src/com/weixin/store/dao/Imp/OrderLineItemDaoImp.java
+++ src/com/weixin/store/dao/Imp/OrderLineItemDaoImp.java
@@ -1,4 +1,33 @@
 package com.weixin.store.dao.Imp;
 
-public class OrderLineItemDaoImp {
+import com.weixin.store.dao.OrderLineItemDao;
+import com.weixin.store.domain.OrderLineItem;
+
+import java.util.List;
+
+public class OrderLineItemDaoImp implements OrderLineItemDao {
+    @Override
+    public OrderLineItem findByPK(long pk) {
+        return null;
+    }
+
+    @Override
+    public List<OrderLineItem> findAll() {
+        return null;
+    }
+
+    @Override
+    public void create(OrderLineItem orderLineItem) {
+
+    }
+
+    @Override
+    public void modify(OrderLineItem orderLineItem) {
+
+    }
+
+    @Override
+    public void remove(String pk) {
+
+    }
 }
